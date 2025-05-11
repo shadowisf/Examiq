af1ff9a59a3aac9ad6ec85cfcfec017f6f3d9822
shadowisf <lespaul021504@gmail.com>
2025-05-11 14:31:44 +0400

    added suspense on signin

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import { readCurrentUser } from "../utils/default/readEntities";
 import SignInForm from "./components/SignInForm";
 
@@ -5,6 +6,11 @@ export default async function SignIn() {
   const { currentUser, currentUserError } = await readCurrentUser();
 
   return (
-    <SignInForm currentUser={currentUser} currentUserError={currentUserError} />
+    <Suspense fallback={<div />}>
+      <SignInForm
+        currentUser={currentUser}
+        currentUserError={currentUserError}
+      />
+    </Suspense>
   );
 }
